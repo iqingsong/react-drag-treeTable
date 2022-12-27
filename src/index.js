1de648bbe520f04665dc037391626bfa30050eda
iqingsong <1483092580@qq.com>
2022-12-27 17:20:46 +0800

    formatting code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import moment from 'moment'
 import 'moment/locale/zh-cn'
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.css'
 
 moment.locale('zh-cn')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
+)
